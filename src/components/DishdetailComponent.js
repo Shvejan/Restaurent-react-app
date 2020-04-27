e0d3b005523af25c70f8b0d093b36ae9b36ffa56
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-04-27 10:28:29 +0530

    ass3 done

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,11 +7,36 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Row,
+  Col,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Label,
 } from "reactstrap";
-
+import { LocalForm, Control, Errors, controls } from "react-redux-form";
 import { Link } from "react-router-dom";
-
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length > len;
+const required = (val) => val && val.length && val != "--Rating--";
 class DishDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModelOpen: false,
+    };
+  }
+  togleModel = () => {
+    this.setState({
+      isModelOpen: !this.state.isModelOpen,
+    });
+    console.log(this.state.isModelOpen);
+  };
+  handleSubmit = (valuse) => {
+    this.togleModel();
+    alert(JSON.stringify(valuse));
+  };
   renderDish(dish) {
     if (dish != null)
       return (
@@ -30,7 +55,6 @@ class DishDetails extends Component {
     else return <div></div>;
   }
   renderComments(comments) {
-    console.log(comments);
     if (comments != null) {
       return (
         <div className="col-12 col-md-5 m-1" key={comments}>
@@ -51,13 +75,111 @@ class DishDetails extends Component {
               })}
             </ul>
           </div>
+          <Row className="form-group">
+            <Col md={{ size: 10 }}>
+              <Button type="submit" onClick={this.togleModel}>
+                <i class="fa fa-pencil" aria-hidden="true" /> Submit Comment
+              </Button>
+            </Col>
+          </Row>
         </div>
       );
     } else return <div></div>;
   }
+
   render() {
     return (
       <div className="container">
+        <div className="justify-content-center">
+          <Modal isOpen={this.state.isModelOpen} toggle={this.togleModel}>
+            <ModalHeader toggle={this.togleModel}>Submit Comment</ModalHeader>
+            <ModalBody>
+              <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <Row className="form-group">
+                  <Label htmlFor="rating" md={{ size: 12, offset: 1 }}>
+                    Rating
+                  </Label>
+                  <Col md={{ size: 10, offset: 1 }}>
+                    <Control.select
+                      model=".rating"
+                      id="rating"
+                      name="rating"
+                      className="form-control"
+                      default="5"
+                      validators={{ required }}
+                    >
+                      <option>--Rating--</option>
+                      <option>5</option>
+                      <option>4</option>
+                      <option>3</option>
+                      <option>2</option>
+                      <option>1</option>
+                    </Control.select>
+                    <Errors
+                      className="text-danger"
+                      model=".rating"
+                      show="touched"
+                      messages={{
+                        required: "Required",
+                      }}
+                    />
+                  </Col>
+                </Row>
+
+                <Row className="form-group">
+                  <Label htmlFor="name" md={{ size: 12, offset: 1 }}>
+                    Name
+                  </Label>
+                  <Col md={{ size: 10, offset: 1 }}>
+                    <Control.text
+                      model=".name"
+                      id="name"
+                      name="name"
+                      placeholder="Name"
+                      className="form-control"
+                      validators={{
+                        required,
+                        minLength: minLength(2),
+                        maxLength: maxLength(15),
+                      }}
+                    />
+                    <Errors
+                      className="text-danger"
+                      show="touched"
+                      model=".name"
+                      messages={{
+                        required: "Required ",
+                        maxLength: "length must be less than 15 characters",
+                        minLength: "length must be more than 2 characters",
+                      }}
+                    />
+                  </Col>
+                </Row>
+                <Row className="form-group">
+                  <Label htmlFor="comment" md={{ size: 12, offset: 1 }}>
+                    Comment
+                  </Label>
+                  <Col md={{ size: 10, offset: 1 }}>
+                    <Control.textarea
+                      model=".comment"
+                      id="comment"
+                      name="comment"
+                      rows="5"
+                      className="form-control"
+                    />
+                  </Col>
+                </Row>
+                <Row className="form-group">
+                  <Col md={{ size: 10, offset: 1 }}>
+                    <Button color="primary" type="submit">
+                      Submit
+                    </Button>
+                  </Col>
+                </Row>
+              </LocalForm>
+            </ModalBody>
+          </Modal>
+        </div>
         <div className="row">
           <Breadcrumb>
             <BreadcrumbItem>

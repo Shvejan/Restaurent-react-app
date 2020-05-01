4b625b3966a6cb791be2b679e8932256183c206b
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-05-01 17:09:45 +0530

    Updates

---
+++ precache-manifest.302fda56d89cd6f379c15bbf6f370f88.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98673cbf01a4a7eb8f2b39904dad19e8",
+    "url": "/Restaurent-react-app/index.html"
+  },
+  {
+    "revision": "e55faa97dfd21871a68c",
+    "url": "/Restaurent-react-app/static/css/2.c6086962.chunk.css"
+  },
+  {
+    "revision": "d08f48e57c1779f0f071",
+    "url": "/Restaurent-react-app/static/css/main.616d76b1.chunk.css"
+  },
+  {
+    "revision": "e55faa97dfd21871a68c",
+    "url": "/Restaurent-react-app/static/js/2.d043f944.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Restaurent-react-app/static/js/2.d043f944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08f48e57c1779f0f071",
+    "url": "/Restaurent-react-app/static/js/main.8af561f9.chunk.js"
+  },
+  {
+    "revision": "c4a33b036e8991382068",
+    "url": "/Restaurent-react-app/static/js/runtime-main.201c9f6b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restaurent-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restaurent-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restaurent-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restaurent-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restaurent-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
